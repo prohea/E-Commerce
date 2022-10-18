0efbc9e49fed704c06f4cc4fecd3886950f69d2c
Mizuri <pro.1.h@outlook.com>
2022-10-18 13:24:48 -0600

    updated model index

--- db/models/index.js
+++ db/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne( {
+  foreign: '',
+  onDelete: '',
+});
 
 // Categories have many Products
+Category.belongsTo( {
+  foreignKey: '',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.hasMany( {
+  foreignKey: '',
+  onDelete: '',
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsTo( {
+  foreignKey: '',
+});
 
 module.exports = {
   Product,
