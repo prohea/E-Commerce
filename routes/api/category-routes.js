0301a759a6da94145fc1c1d8638f6aa9b0ac48e5
Mizuri <pro.1.h@outlook.com>
2022-10-18 15:48:01 -0600

    fixed routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,12 +10,10 @@ router.get('/', async (req, res) => {
       include: [Product],
         // be sure to include its associated Products
     });
-
     res.status(200).json(categoriesData);
   } catch (err) {
     res.status(500).json(err);
   }
-  
 });
 
 router.get('/:id', async (req, res) => {
@@ -25,6 +23,10 @@ router.get('/:id', async (req, res) => {
       // be sure to include its associated Products
       include: [Product],
     });
+    if (!categoryData {
+      res.status(404).json({ message: 'No category' });
+      return;
+    }
     res.status(200).json(categoryData);
   } catch (err) {
       res.status(500).json(err);
@@ -34,15 +36,31 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create(req.body);
+    const categoryData = await Category.create({
+      category_id: req.body,
+    });
   res.status(200).json(categoryData);
 } catch (err) {
   res.status(400).json(err);
-},
+}
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+  if (!categoryData[0]){
+    res.status(404).json({ message: 'No category id' });
+    return;
+  }
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async(req, res) => {
@@ -51,11 +69,12 @@ router.delete('/:id', async(req, res) => {
     const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
-      },
+      }
     }); 
   res.status(200).json(categoryData);
 } catch (err) {
   res.status(400).json(err);
+}
 });
 
 module.exports = router;
