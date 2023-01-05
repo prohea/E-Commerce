e3827a926c3ef9860ec4df4b2cb5ef8a1ad0e32f
Mizuri <pro.1.h@outlook.com>
2023-01-05 13:50:13 -0700

    downloaded packages

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const routes = require('./fantastic-umbrella/Develop/routes');
+const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
