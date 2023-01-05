e317ef2afd649f5884ae6443db431d9a2142126d
Mizuri <pro.1.h@outlook.com>
2023-01-05 15:27:45 -0700

    updaded index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const apiRoutes = require('./api');
+const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
@@ -7,4 +7,4 @@ router.use((req, res) => {
 	res.send("<h1>Wrong Route!</h1>");
 });
 
-module.exports= router;
+module.exports = router;
